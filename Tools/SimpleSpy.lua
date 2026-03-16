b1754a35a56b33c0e87bad09544226603a4cfe57
XiaoYunUwU <XiaoYunUwU@noreply.gitcode.com>
2026-03-16 20:44:23 +0800

    update: 更新文件 SimpleSpy.lua
    Signed-off-by: XiaoYunUwU <XiaoYunUwU@noreply.gitcode.com>

--- Tools/SimpleSpy.lua
+++ Tools/SimpleSpy.lua
@@ -189,7 +189,7 @@ function ErrorPrompt(Message)
 end
 
 local Highlight = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/Highlight.lua"))()
-local Serialize = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xingtaiduan/Script/refs/heads/main/Tools/Serializer.lua"))()
+local Serialize = loadstring(game:HttpGet("https://raw.gitcode.com/XiaoYunUwU/XA/raw/main/Tools/Serializer.lua"))()
 
 local SimpleSpy3 = Create("ScreenGui",{
     Name = "SimpleSpy",
