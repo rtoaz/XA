447d6a1849228267ab2fd5743ac48017722e5f93
XiaoYunUwU <XiaoYunUwU@noreply.gitcode.com>
2026-03-17 20:10:57 +0800

    update: 更新文件 text.lua
    Signed-off-by: XiaoYunUwU <XiaoYunUwU@noreply.gitcode.com>

--- text.lua
+++ text.lua
@@ -1,5 +1,5 @@
 game.StarterGui:SetCore("SendNotification", {
-    Title = "公告",
-    Text = "大家好呀，小云在这里～",
+    Title = "XA公告",
+    Text = "请大家帮忙宣传XAHub，它是完全免费的公益脚本，非常感谢支持！",
     Duration = 3
 })
